1d269e77f47c988634d738ee07537cd368eb5bac
nathanieltornow <nate.tornow@gmail.com>
2021-05-26 16:48:24 +0200

    Also send num of records as response

--- shard/shard_rpc.go
+++ shard/shard_rpc.go
@@ -8,37 +8,37 @@ import (
 )
 
 func (s *Shard) Append(ctx context.Context, request *pb.AppendRequest) (*pb.CommittedRecord, error) {
-	s.diskMu.Lock()
 	lsn, err := s.disk.Write(request.Record)
-	s.diskMu.Unlock()
+
 	if err != nil {
 		return nil, err
 	}
-	s.lsn += 1
+	s.snMu.Lock()
+	s.sn += 1
+	s.snMu.Unlock()
 	var gsn int64
 	if !s.isRoot {
-		logrus.Infoln("hi")
 		gsn = s.WaitForGsn(lsn)
-		logrus.Infoln("hi2")
 	} else {
-		gsn = s.lsn
+		gsn = s.sn
 	}
-
-	s.diskMu.Lock()
 	err = s.disk.Sync()
 	if err != nil {
+		logrus.Infoln("1")
+
 		return nil, err
 	}
 	err = s.disk.Commit(lsn, gsn)
 	if err != nil {
+		logrus.Infoln("2")
+
 		return nil, err
 	}
 	err = s.disk.Sync()
 	if err != nil {
+		logrus.Infoln("3")
 		return nil, err
 	}
-	s.diskMu.Unlock()
-
 	return &pb.CommittedRecord{Gsn: gsn, Record: request.Record}, nil
 }
 
@@ -46,7 +46,7 @@ func (s *Shard) GetOrder(stream pb.Shard_GetOrderServer) error {
 	if s.isRoot && s.isSequencer {
 		for {
 			req, err := stream.Recv()
-			logrus.Infoln(req.StartLsn, req.NumOfRecords)
+			logrus.Infoln("Received OrderReq", req.StartLsn, req.NumOfRecords)
 			if err == io.EOF {
 				return nil
 			}
@@ -54,10 +54,10 @@ func (s *Shard) GetOrder(stream pb.Shard_GetOrderServer) error {
 				return err
 			}
 
-			s.lsnMu.Lock()
-			res := pb.OrderResponse{StartGsn: s.lsn, StartLsn: req.StartLsn}
-			s.lsn += req.NumOfRecords
-			s.lsnMu.Unlock()
+			s.snMu.Lock()
+			res := pb.OrderResponse{StartGsn: s.sn, StartLsn: req.StartLsn, NumOfRecords: req.NumOfRecords}
+			s.sn += req.NumOfRecords
+			s.snMu.Unlock()
 
 			if err := stream.Send(&res); err != nil {
 				return err
